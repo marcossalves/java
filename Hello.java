3b6155267b4b59ede782e646947671b0bc5411e7
marcos <marcossa.ti@terra.com.br>
2020-05-06 12:00:35 -0300

    Exercícios Fundamentos,Hello World e Media

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+ * @author Marcos Severino Alves
+ * Hello World
+ *
+ */
+package cursoJava;
+
+/**
+ * @author Marcos Severino Alves
+ * Hello World
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
